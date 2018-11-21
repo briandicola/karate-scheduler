5e5380804a2381043a52a491864b527fbfa213f1
briandicola <brian.dicola@accenture.com>
2018-11-21 14:51:42 -0500

    Rearranged files.  Changed index.js to handler.js

---
+++ serverless-project/belt-ranges.js
@@ -0,0 +1,10 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'BELTS': {
+    'white-yellow': ['white', 'white advanced', 'yellow'],
+    'gold-purple': ['gold', 'orange', 'green', 'purple'],
+    'blue-black': ['blue', 'brown', 'red', 'cho dan bo', 'black red','red black', 'black']
+  },
+};
---
+++ serverless-project/students.js
@@ -0,0 +1,13 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'STUDENTS':{
+    'emily':[
+      '4:00',
+      '5:00',
+      '5:45',
+      '7:00'
+    ]
+  },
+};
